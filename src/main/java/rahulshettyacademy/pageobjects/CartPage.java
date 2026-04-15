27de65412b7660c64e83a1aca0bd0ca417107f49
chandan <konar.chandan@gmail.com>
2026-04-16 00:40:09 +0530

    Initial files commit

---
+++ src/main/java/rahulshettyacademy/pageobjects/CartPage.java
@@ -0,0 +1,47 @@
+package rahulshettyacademy.pageobjects;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import rahulshettyacademy.AbstractComponents.AbstractComponent;
+
+
+public class CartPage extends AbstractComponent {
+	WebDriver driver;
+
+	@FindBy(css = ".totalRow button")
+	WebElement checkoutEle;
+
+	@FindBy(css = ".cartSection h3")
+	private List<WebElement> cartProducts;
+	
+	By checkoutButton = By.cssSelector(".totalRow button");
+
+	public CartPage(WebDriver driver) {
+		super(driver);
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+
+	}
+
+	public Boolean VerifyProductDisplay(String productName) {
+		Boolean match = cartProducts.stream().anyMatch(product -> product.getText().equalsIgnoreCase(productName));
+		return match;
+
+	}
+
+	public CheckoutPage goToCheckout() {
+		windowScrollHeight();
+		waitForElementToAppear(checkoutButton);
+		checkoutEle.click();
+		return new CheckoutPage(driver);
+		
+
+	}
+
+}
