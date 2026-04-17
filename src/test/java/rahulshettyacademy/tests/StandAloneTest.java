c143067270a50c8d049445e671f9f933bdab03e6
chandan <konar.chandan@gmail.com>
2026-04-17 17:45:13 +0530

    updated3 on 17042026

--- src/test/java/rahulshettyacademy/tests/StandAloneTest.java
+++ src/test/java/rahulshettyacademy/tests/StandAloneTest.java
@@ -20,7 +20,7 @@ public class StandAloneTest {
 
 	public static void main(String[] args) throws InterruptedException {
 		// TODO Auto-generated method stub
-
+                  //practice cicd
 		String productName = "ZARA COAT 3";
 		//WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
